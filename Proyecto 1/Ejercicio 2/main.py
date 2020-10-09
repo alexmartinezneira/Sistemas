4eee09009dc95aa2274c8058af879604b18bda7c
Alex <alex.martinez2017@alu.uct.cl>
2020-10-09 12:32:13 -0300

    Autimatizacion del ejercicio 2
    ahora si que lo termine

--- Proyecto 1/Ejercicio 2/main.py
+++ Proyecto 1/Ejercicio 2/main.py
@@ -17,7 +17,7 @@ def Trama(n):
                 if((array[i]!=0)):
                     data+=pow(2,i-2)
                     
-        return (data)                                       #retorna el dato de forma decimal 
+        return (hex(data))                                       #retorna el dato de forma decimal 
 
     def Direccion(array):                                   #Funcion para ver en que direccion de memoria se va a escribir o leer el data
         data=0
@@ -27,13 +27,13 @@ def Trama(n):
                 if((array[i]!=0)):
                     data+=pow(2,i-10)
                     
-        return (data)                                       #retorna el dato de forma decimal 
+        return hex(data)                                       #retorna el dato de forma decimal 
     
     def Operacion(array):                                   #Funcion para obtener la operacion que se va a realizar 0 para Leer 1 para Escribir
     
        array.reverse()
        data=array[18]
-       return (data)
+       return hex(data)
     
     def Device(array):                                      #Funcion para ver que dispositivo se va a utilizar 
         data=0
@@ -42,18 +42,22 @@ def Trama(n):
             if(i>=19 and i<=21):
                 if((array[i]!=0)):
                     data+=pow(2,i-19)
-        return (((data)))
+        return hex(data)
     
     return Data(array),Direccion(array),Operacion(array),Device(array)      #Retorna todas las funciones anidadas 
     
 
-n=int(input('Ingrese cualquier trama de datos en base 10: '))               #Se ingresa el numero en su  forma decimal 
+n=[179900835,177765503,179570763,177904163]             #Se ingresa el numero en su  forma decimal 
 
-
-print('El valor del dato es: '+str(Trama(n)[0]))                            #Se manda a llamar a la Funcion Trama y devuelve la funcion Anidada Data 
-print('El valor de la direccion de memoria es: '+str(Trama(n)[1]))          #Se manda a llamar a la Funcion Trama y devuelve la funcion Anidada Direccion 
-print('El valor de la operacion  es: '+str(Trama(n)[2]))                    #Se manda a llamar a la Funcion Trama y devuelve la funcion Anidada Operacion 
-print('El valor del dispositivo  es: '+str(Trama(n)[3]))                    ##Se manda a llamar a la Funcion Trama y devuelve la funcion Anidada Device  
+for i in range(len(n)):
+    
+    print("+---------------------------------------------------+")
+    print("|   El resultado del numero: "+str(n[i])+'              |')
+    print('|   El valor del dato es: '+str(Trama(n[i])[0])+'                      |')                            #Se manda a llamar a la Funcion Trama y devuelve la funcion Anidada Data 
+    print('|   El valor de la direccion de memoria es: '+str(Trama(n[i])[1])+'    |')          #Se manda a llamar a la Funcion Trama y devuelve la funcion Anidada Direccion 
+    print('|   El valor de la operacion  es: '+str(Trama(n[i])[2])+'               |')                    #Se manda a llamar a la Funcion Trama y devuelve la funcion Anidada Operacion 
+    print('|   El valor del dispositivo  es: '+str(Trama(n[i])[3])+'               |')                    ##Se manda a llamar a la Funcion Trama y devuelve la funcion Anidada Device
+print('+---------------------------------------------------+')
 
     
     
